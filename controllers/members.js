f7f5f3c06f744e9ce77d2f43b4c96cdd7170954a
diegopss <die-gops@hotmail.com>
2021-09-11 19:24:24 -0300

    Ajustando dados de cadastro de membro

--- controllers/members.js
+++ controllers/members.js
@@ -41,20 +41,19 @@ exports.post = function(req, res){
         }
     }
 
-    let { avatar_url, birth, name, services, gender} = req.body
 
-    birth = Date.parse(birth)
+    birth = Date.parse(req.body.birth)
     
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
+    let id = 1 
+    const lastMember = data.members[data.members.length - 1]
+        if (lastMember) {
+            id = lastMember.id + 1
+        }
     
     data.members.push({
         id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        created_at
+        ...req.body,
+        birth
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
